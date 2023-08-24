b6104045e60f613459d4fedccf339b9298febef6
purepani <pani0028@umn.edu>
2023-08-24 17:22:44 -0500

    refactored 2

--- comb/satwik/homeConfigurations/default.nix
+++ comb/satwik/homeConfigurations/default.nix
@@ -7,13 +7,13 @@
       system = "x86_64-linux";
       pkgs = import inputs.nixpkgs {
         inherit system;
-        config.allowUnfree=true;
+        config.allowUnfree = true;
       };
     };
   };
 in
   inputs.hive.findLoad {
     inherit cell;
-    inputs = inputs // {inherit common;};
+    inputs = inputs // {inherit (common) bee;};
     block = ./.;
   }
