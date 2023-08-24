5832848fbf5c7b7495e524d6904ffdd4f6439c53
purepani <pani0028@umn.edu>
2023-08-24 02:06:30 -0500

    test

--- comb/satwik/nixosConfigurations/laptop/default.nix
+++ comb/satwik/nixosConfigurations/laptop/default.nix
@@ -3,22 +3,22 @@
   cell,
 }: let
   inherit (inputs) common;
-  inherit (cell) nixosProfiles hardwareProfiles;
+  inherit (cell) hardwareProfiles;
   hostname = "satwik";
 in {
   inherit (common) bee;
   networking = {inherit hostname;};
-  imports = with nixosProfiles; [
-    hardwareProfiles.laptop 
-    extra
-    nix
-    desktop
-    kdeconnect
-    locale
-    nix
-    pipewire
-    steam
-    virtualization
-    zerotier-one
+  imports = [
+    hardwareProfiles.laptop
+    #extra
+    #nix
+    #desktop
+    #kdeconnect
+    #locale
+    #nix
+    #pipewire
+    #steam
+    #virtualization
+    #zerotier-one
   ];
 }
