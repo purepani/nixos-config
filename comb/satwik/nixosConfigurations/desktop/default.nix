95f5df965c96f192fb9a0e1995bcc96aa524d3bf
purepani <pani0028@umn.edu>
2023-11-11 21:58:39 -0600

    Removes rt-kernel from desktop

--- comb/satwik/nixosConfigurations/desktop/default.nix
+++ comb/satwik/nixosConfigurations/desktop/default.nix
@@ -10,7 +10,7 @@ in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages-rt_latest;
+  #boot.kernelPackages = pkgs.linuxPackages-rt_latest;
   imports = with nixosProfiles; [
     hardwareProfiles.desktop
     extra
