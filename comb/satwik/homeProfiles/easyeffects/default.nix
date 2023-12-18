ae52d2d91557d67d008a7de318bdf133801beb12
purepani <pani0028@umn.edu>
2023-12-18 13:26:46 -0600

    Adds deepfilter to easyeffects

--- comb/satwik/homeProfiles/easyeffects/default.nix
+++ comb/satwik/homeProfiles/easyeffects/default.nix
@@ -4,8 +4,13 @@
 }: {
   services.easyeffects = {
     enable = true;
-    package = inputs.nixpkgs.easyeffects.override {
-      speexdsp = inputs.nixpkgs.speexdsp.overrideAttrs (_: {configureFlags = [];});
-    };
+    #package =
+    #  (inputs.nixpkgs.easyeffects.overrideAttrs (old: {
+    #    buildInputs = old.buildInputs ++ [cell.packages.deepfilter.package];
+    #  }))
+    #  .override {
+    #    speexdsp = inputs.nixpkgs.speexdsp.overrideAttrs (_: {configureFlags = [];});
+    #  };
+    package = cell.packages.easyeffects.package;
   };
 }
--- comb/satwik/packages/default.nix
+++ comb/satwik/packages/default.nix
@@ -6,8 +6,11 @@
 in
   inputs.hive.load {
     src = ./.;
-    inputs = {
-      inherit inputs cell;
-    } // (removeAttrs pkgs ["self" "super" "root"]);
+    inputs =
+      {
+        inherit inputs cell;
+        inherit (cell.packages) deepfilter;
+      }
+      // (removeAttrs pkgs ["self" "super" "root"]);
     loader = inputs.hive.loaders.callPackage;
   }
