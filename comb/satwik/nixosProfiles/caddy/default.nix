b7058e3b29d4233824a0c0a6e9082101f18b95b2
purepani <pani0028@umn.edu>
2024-02-03 15:45:26 -0600

    Adds Godaddy DNS module to caddy

--- comb/satwik/nixosProfiles/caddy/default.nix
+++ comb/satwik/nixosProfiles/caddy/default.nix
@@ -8,6 +8,18 @@
 
   services.caddy = {
     enable = true;
+    package = cell.packages.caddy.override {
+      externalPlugins = [
+        {
+          name = "godaddy";
+          repo = "github.com/caddy-dns/godaddy";
+          version = "7634a752ba5bda3977b461c60f5936eba3d02426";
+          #version = "1.0.3";
+          #version = "8789126791ed250b532e1d7d512256737625e6e0";
+        }
+      ];
+      vendorHash = "sha256-hVRMAzLqHo8dk2ni1bZvoJwrjWhCeRNS9+JDZB/eYus=";
+    };
     virtualHosts = {
       "http://veneprodigy.com" = {
         extraConfig = ''
@@ -15,7 +27,7 @@
         '';
         #serverAlias = ["www.veneprodigy.com"];
       };
-            
+
       "http://jellyfin.veneprodigy.com" = {
         extraConfig = ''
           reverse_proxy 127.0.0.1:8086
