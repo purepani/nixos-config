2f7c2d1db60c487b09e8da2d6deb6829c01dca24
purepani <pani0028@umn.edu>
2024-09-06 15:56:19 -0500

    Change wezterm tab font

--- comb/satwik/homeProfiles/wezterm/default.nix
+++ comb/satwik/homeProfiles/wezterm/default.nix
@@ -2,16 +2,20 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-                      return {
-            	      	front_end = "WebGpu",
-      		font_size = 8.0,
-            		window_padding = {
-      			left = 0,
-      			right = 0,
-      			top = 0,
-      			bottom=0,
-            			}
-                      }
+                            return {
+                  	      	front_end = "WebGpu",
+            			font_size = 8.0,
+                  		window_padding = {
+      				left = 0,
+      				right = 0,
+      				top = 0,
+      				bottom=0,
+      			},
+      			window_frame = {
+      				font_size=8.0,
+      			},
+                            },
+      			
     '';
   };
 }
